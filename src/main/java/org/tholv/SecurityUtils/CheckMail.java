33776d9f186522bedf77947fea64f10de656a96b
Your Name <you@example.com>
2022-08-29 16:27:00 +0700

    'tholv'

--- src/main/java/org/tholv/SecurityUtils/CheckMail.java
+++ src/main/java/org/tholv/SecurityUtils/CheckMail.java
@@ -16,25 +16,21 @@ public class CheckMail {
         throw new IllegalArgumentException("Email is not valid");
     }
 
-    public synchronized boolean checkMailClone(String emailClone,String emailRoot){
+    public synchronized boolean isEmailClone(String emailClone,String emailRoot){
         CheckMail checkMail = this.getInstance();
         if(emailClone.equals(emailRoot)){
             throw  new IllegalArgumentException("Email clone equals email root");
         }
         emailClone.trim();
-        if(!(checkMail.checkEmail(emailClone)  && checkMail.checkEmail(emailRoot))){
+        if(!(checkMail.checkEmail(emailClone)  && checkMail.checkEmail(emailRoot)))
             throw new IllegalArgumentException("Email is not valid");
-        }
-        if(emailClone.contains("+")){
-            throw new IllegalArgumentException("Email is not valid");
-        }
+        if(emailClone.contains("+"))throw new IllegalArgumentException("Email is not valid");
         String str="";
-        String[] emailCloneSplit = emailClone.substring(0,emailClone.lastIndexOf("@")).split(".");
-        str= StringUtils.concat(emailCloneSplit);
+        String[] emailCloneSplit = emailClone.substring(0,emailClone.lastIndexOf("@")).split("[.]");
+        str= StringUtils.concat(emailCloneSplit).trim();
         if(str.equals(emailRoot.substring(0,emailRoot.lastIndexOf("@")))){
-            throw new IllegalArgumentException("Email is exist");
+            return true;
         }
-    return true;
+        return false;
     }
-
 }
